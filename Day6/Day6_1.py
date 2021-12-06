783c634656f32ebc3622e833bdf06e8f58496b9c
D3h4n <75465982+D3h4n@users.noreply.github.com>
2021-12-06 00:57:44 -0500

    Add comments to solutions

--- Day6/Day6_1.py
+++ Day6/Day6_1.py
@@ -4,15 +4,18 @@ DAYS = 80
 
 def main(args):
    with open("test_input.txt" if len(args) > 1 and args[1] == "-t" else "input.txt") as f:
+      # generate intial list of fish
       fish_list = list(map(lambda x: int(x), f.readline().split(",")))
       
       new_fish_count = 0
 
-      for x in range(0, DAYS + 1):
+      # iterate through n days
+      for x in range(DAYS + 1):
          # add new fish to fish_list at the start of the new day
          fish_list.extend([8] * new_fish_count)
          new_fish_count = 0
 
+         # simulate day for each fish
          for i, fish in enumerate(fish_list):
             if (fish == 0):
                fish = 7
@@ -22,7 +25,7 @@ def main(args):
 
             fish_list[i] = fish
          
-
+      # print the number of fish after n days
       print(len(fish_list))
 
 if __name__ == "__main__":
--- Day6/Day6_2.py
+++ Day6/Day6_2.py
@@ -1,9 +1,10 @@
 import sys
 
-DAYS = 256 
+DAYS = 18 
 
 def main(args):
    with open("test_input.txt" if len(args) > 1 and args[1] == "-t" else "input.txt") as f:
+      # map of fish timer -> number of fish
       fish_map = {
          0: 0,
          1: 0,
@@ -16,28 +17,24 @@ def main(args):
          8: 0
       }
 
+      # initialise map
       for fish in map(lambda x: int(x), f.readline().split(",")):
          fish_map[fish] += 1
 
+      # iterate for n days
       for x in range(DAYS):
-         print(f"Day {x}", list(fish_map.values()))
-         
+         # handle fish reproducing
          new_fish = fish_map[0]
-         fish_map[0] = 0
          fish_map[7] += new_fish
 
+         # iterate through other stages and iterate fish timers
          for timer in range(1, 9):
-            fish = fish_map[timer]
-            fish_map[timer] = 0
-            fish_map[timer - 1] = fish
+            fish_map[timer - 1] = fish_map[timer]
 
+         # add new fish after day
          fish_map[8] = new_fish
 
-      print(f"Day {DAYS}", list(fish_map.values()))
-      num_fish = sum(fish_map.values())
-      print(num_fish)
-            
-
+      print(sum(fish_map.values())) # print the total number of fish
 
 if __name__ == "__main__":
    main(sys.argv)
